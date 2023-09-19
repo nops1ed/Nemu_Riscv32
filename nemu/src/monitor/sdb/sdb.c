329d65f46213f8b154e84a345a5c5b507ddca4e5
tracer-ics2023 <tracer@njuics.org>
2023-09-19 21:59:34 +0800

    >  compile NEMU SA23225330 原神启动 Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:59:34 up  4:55,  1 user,  load average: 0.16, 0.05, 0.01

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -55,6 +55,16 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_si(char *args)
+{	
+  //parse args
+  char *arg = strtok(NULL," ");
+  int i = arg ? atoi(arg) : 1;
+  cpu_exec(i);	  
+  return 0;
+}
+
+
 static struct {
   const char *name;
   const char *description;
@@ -63,6 +73,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si" , "Execute by single step" , cmd_si },
 
   /* TODO: Add more commands */
 
