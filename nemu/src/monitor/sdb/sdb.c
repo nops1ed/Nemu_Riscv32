dbc0ab17f40005feef352108b68d4cfa46f5c6f8
tracer-ics2023 <tracer@njuics.org>
2023-09-21 10:36:14 +0800

    >  compile NEMU SA23225330 原神启动 Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:36:14 up 0 min,  1 user,  load average: 0.20, 0.06, 0.02

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -109,7 +109,7 @@ static int cmd_x(char *args) {
 
   for (int j = 0 ; j < i ; j++)
   {
-    printf("0x%lx: %10x\n" , addr + 4 * j, vaddr_read(addr + 4 * j , 4));
+    printf("0x%lx: %010x\n" , addr + 4 * j, vaddr_read(addr + 4 * j , 4));
   }
   return 0;
 }
