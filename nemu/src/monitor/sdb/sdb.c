ce0becaaba1b0413c05280941c53cba3d8101bef
tracer-ics2023 <tracer@njuics.org>
2023-09-21 12:55:00 +0800

    >  compile NEMU SA23225330 原神启动 Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:55:00 up  2:19,  1 user,  load average: 0.03, 0.03, 0.00

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -19,8 +19,7 @@
 #include <readline/history.h>
 #include "sdb.h"
 
-//#include <memory/vaddr.h>
-#include <memory/paddr.h>
+#include <memory/vaddr.h>
 
 static int is_batch_mode = false;
 
@@ -103,7 +102,7 @@ static int cmd_x(char *args) {
     printf("x: No vaild Args\n");
     return 0;
   }
-  paddr_t addr;
+  vaddr_t addr;
   //printf("%s\n" , arg);
   sscanf(arg, "%x" , &addr);
   printf("%x\n" , addr);
@@ -111,7 +110,7 @@ static int cmd_x(char *args) {
 
   for (int j = 0 ; j < i ; j++)
   {
-    printf("0x%x: %08x\n" , addr + 4 * j, paddr_read(addr + 4 * j , 4));
+    printf("0x%x: %08x\n" , addr + 4 * j, vaddr_read(addr + 2 * j , 4));
   }
   return 0;
 }
