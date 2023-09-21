fc468755e79ef3e4afd70dc38a4d046a9255d962
tracer-ics2023 <tracer@njuics.org>
2023-09-21 10:03:57 +0800

    >  compile NEMU SA23225330 原神启动 Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:03:57 up  8:25,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -103,7 +103,7 @@ static int cmd_x(char *args) {
   }
   uint64_t addr;
   printf("%s\n" , arg);
-  sscanf(arg + 2, "%ld" , &addr);
+  sscanf(arg + 2, "%lx" , &addr);
   printf("%lu\n" , addr);
 	printf("-----------------------------------\n");
 
