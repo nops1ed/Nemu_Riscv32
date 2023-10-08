da6d3a405d74789f7e4bc87f37d93f3a64b06877
tracer-ics2023 <tracer@njuics.org>
2023-10-08 23:12:56 +0800

    >  compile NEMU SA23225330 Nops1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:12:56 up  6:02,  1 user,  load average: 0.09, 0.03, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -49,7 +49,7 @@ static struct rule {
 	{"\\(", TK_LBT},					// left bracket
 	{"\\)", TK_RBT},					// right bracket
 	{"\\[$]{1}\\w{1,}" , TK_REG},		// register
-	{"\\[0-9]+" , TK_DEC},			// decimal number								
+	{"[0-9]+" , TK_DEC},			// decimal number								
 	{"\\0\\x\\d{1,}",TK_HEX}, 		// hex number
 	{"==", TK_EQ},						// equal
 	{"!=" , TK_NEQ},					// bool not equal
@@ -147,7 +147,7 @@ static bool make_token(char *e) {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
       return false;
     }
-  }
+}
 
   return true;
 }
