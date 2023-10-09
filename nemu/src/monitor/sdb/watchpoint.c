9789ada11e66238b0570b2186159b9fe9fefa56d
tracer-ics2023 <tracer@njuics.org>
2023-10-10 07:27:59 +0800

    >  compile NEMU SA23225330 Nops1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  07:27:59 up 16:22,  1 user,  load average: 0.01, 0.00, 0.00

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -59,12 +59,12 @@ void display_wp (void) {
 
 WP* new_wp() {
 	if (!free_)	return NULL;
+	puts("new");
 	WP *_tmp;
 	for (_tmp = head; _tmp -> next != NULL ; _tmp = _tmp -> next);	
 	_tmp -> next = free_;
 	free_ = free_ -> next;
 	_tmp -> next -> next = NULL;
-	puts("here ");
 	return _tmp -> next;
 }
 
