8583882b23e4d98382e428eed0758205473b8e5c
tracer-ics2023 <tracer@njuics.org>
2023-10-09 16:28:20 +0800

    >  compile NEMU SA23225330 Nops1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:28:20 up  1:23,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -249,7 +249,8 @@ static uint32_t eval(int p , int q) {
     	 * val2 = eval(op + 1, q);
 		 */
 		uint32_t op = domain_find(p , q);
-		printf("\nThe domain OPERATION could be %d \n" , tokens[op].type);
+		printf("\nThe domain OPERATION could be %d \n" , op);
+		//printf("\nThe domain OPERATION could be %d \n" , tokens[op].type);
 		if (op == -1) assert(0);
 		uint32_t val1 = eval(p , op - 1);
 		uint32_t val2 = eval(op + 1 , q);
