6631214d93bed0a5a8e4ac33e2b64ee08bf18460
tracer-ics2023 <tracer@njuics.org>
2023-10-09 15:21:22 +0800

    >  compile NEMU SA23225330 Nops1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:21:22 up 16 min,  1 user,  load average: 0.00, 0.02, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -119,7 +119,7 @@ static bool make_token(char *e) {
 					//It may cause buffer overflow
 					strncpy(tokens[nr_token].str , substr_start , len);
 					//tokens[nr_token].str[len] = '\0';
-					printf("\nNow we got %s\n" , tokens[nr_token].str);
+					//printf("\nNow we got %s\n" , tokens[nr_token].str);
 					tokens[nr_token++].type = rules[i].token_type;
 					break;
 				case TK_REG:
