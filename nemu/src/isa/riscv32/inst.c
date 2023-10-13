56fac2a5e3e8c19926b6d456b6ab43804e1a59a7
tracer-ics2023 <tracer@njuics.org>
2023-10-13 22:50:55 +0800

    >  compile NEMU SA23225330 Nops1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:50:55 up  3:00,  1 user,  load average: 0.10, 0.11, 0.09

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -23,14 +23,13 @@
 #define Mw vaddr_write
 
 enum {
-  TYPE_I, TYPE_LI, TYPE_U, TYPE_S, TYPE_J, TYPE_R, TYPE_B,
+  TYPE_I, TYPE_U, TYPE_S, TYPE_J, TYPE_R, TYPE_B,
   TYPE_N, // none
 };
 
 #define src1R() do { *src1 = R(rs1); } while (0)
 #define src2R() do { *src2 = R(rs2); } while (0)
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
-#define immLI() do { *imm = BITS(i, 25, 20); } while(0)
 #define immJ() do { *imm = SEXT(BITS(i, 31, 31), 1) << 20 | BITS(i, 30, 21) << 1 \
                       | BITS(i, 20, 20) << 11 | BITS(i, 19, 12) << 12; } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
@@ -46,7 +45,6 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
   *rd     = BITS(i, 11, 7);
   switch (type) {
     case TYPE_I: src1R();          immI(); break;
-    case TYPE_LI:src1R();          immLI(); break;
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
     case TYPE_J:                   immJ(); break;
