9df96b463300c444af4a05a089c2b7fae16133a1
tracer-ics2023 <tracer@njuics.org>
2023-10-17 16:16:54 +0800

    >  compile NEMU SA23225330 Nops1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:16:54 up  1:45,  1 user,  load average: 0.07, 0.06, 0.04

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -23,7 +23,7 @@ word_t vaddr_ifetch(vaddr_t addr, int len) {
 word_t vaddr_read(vaddr_t addr, int len) {
 #ifdef CONFIG_MTRACE
   //Log("Memory Read: " FMT_PADDR "at pc: " FMT_WORD, addr, cpu.pc);
-  printf("Memory Read: 0x%x at pc: 0x%x", addr, cpu.pc);
+  printf("Memory Read: 0x%x at pc: 0x%x\n", addr, cpu.pc);
 #endif
   return paddr_read(addr, len);
 }
