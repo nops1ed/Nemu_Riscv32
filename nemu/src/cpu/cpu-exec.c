2e5977a3179b9e92ad733fd4ef80c0ad100848eb
tracer-ics2023 <tracer@njuics.org>
2023-10-17 14:54:27 +0800

    >  compile NEMU SA23225330 Nops1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:54:27 up 23 min,  1 user,  load average: 0.12, 0.11, 0.08

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -104,6 +104,9 @@ static void statistic() {
 }
 
 void assert_fail_msg() {
+  #ifdef CONFIG_ITRACE
+    Display_RingBuffer();
+  #endif
   isa_reg_display();
   statistic();
 }
@@ -129,9 +132,6 @@ void cpu_exec(uint64_t n) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
 
     case NEMU_END: case NEMU_ABORT:
-      #ifdef CONFIG_ITRACE
-        Display_RingBuffer();
-      #endif
       Log("nemu: %s at pc = " FMT_WORD,
           (nemu_state.state == NEMU_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) :
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
