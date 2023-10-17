cdd1380ff1ce0b9b6f9212e8b4dd9be4b2f98ff6
tracer-ics2023 <tracer@njuics.org>
2023-10-17 15:45:26 +0800

    >  compile NEMU SA23225330 Nops1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:45:26 up  1:13,  1 user,  load average: 0.06, 0.11, 0.06

--- nemu/src/utils/disasm.cc
+++ nemu/src/utils/disasm.cc
@@ -105,6 +105,6 @@ extern "C" void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int
   int skip = s.find_first_not_of('\t');
   const char *p = s.c_str() + skip;
   assert((int)s.length() - skip < size);
-  *_len = (int)s.length();
+  *_len = strlen(p);
   strcpy(str, p);
 }
