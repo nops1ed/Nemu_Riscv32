8363094c7b490daed3b70e827182ab6da7b66ba2
tracer-ics2023 <tracer@njuics.org>
2023-10-19 14:21:32 +0800

    >  compile NEMU SA23225330 Nops1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:21:32 up 1 day,  5:00,  1 user,  load average: 0.08, 0.04, 0.03

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -28,47 +28,43 @@ size_t strlen(const char *str) {
      The 0-bits provide holes for carries to fall into.  */
   himagic = 0x80808080L;
   lomagic = 0x01010101L;
-  if (sizeof (longword) > 4)
-    {
+  if (sizeof (longword) > 4) {
       /* 64-bit version of the magic.  */
       /* Do the shift in two steps to avoid a warning if long has 32 bits.  */
-      himagic = ((himagic << 16) << 16) | himagic;
-      lomagic = ((lomagic << 16) << 16) | lomagic;
-    }
+    himagic = ((himagic << 16) << 16) | himagic;
+    lomagic = ((lomagic << 16) << 16) | lomagic;
+  }
   if (sizeof (longword) > 8)
     assert(0);
   /* Instead of the traditional loop which tests each character,
      we will test a longword at a time.  The tricky part is testing
      if *any of the four* bytes in the longword in question are zero.  */
-  for (;;)
-    {
-      longword = *longword_ptr++;
-      if (((longword - lomagic) & ~longword & himagic) != 0)
-	{
-	  /* Which of the bytes was the zero?  If none of them were, it was
-	     a misfire; continue the search.  */
-	  const char *cp = (const char *) (longword_ptr - 1);
-	  if (cp[0] == 0)
-	    return cp - str;
-	  if (cp[1] == 0)
-	    return cp - str + 1;
-	  if (cp[2] == 0)
-	    return cp - str + 2;
-	  if (cp[3] == 0)
-	    return cp - str + 3;
-	  if (sizeof (longword) > 4)
-	    {
-	      if (cp[4] == 0)
-		return cp - str + 4;
-	      if (cp[5] == 0)
-		return cp - str + 5;
-	      if (cp[6] == 0)
-		return cp - str + 6;
-	      if (cp[7] == 0)
-		return cp - str + 7;
-	    }
-	}
-    }
+  for (;;) {
+    longword = *longword_ptr++;
+    if (((longword - lomagic) & ~longword & himagic) != 0) {
+      /* Which of the bytes was the zero?  If none of them were, it was
+        a misfire; continue the search.  */
+      const char *cp = (const char *) (longword_ptr - 1);
+      if (cp[0] == 0)
+        return cp - str;
+      if (cp[1] == 0)
+        return cp - str + 1;
+      if (cp[2] == 0)
+        return cp - str + 2;
+      if (cp[3] == 0)
+        return cp - str + 3;
+      if (sizeof (longword) > 4) {
+        if (cp[4] == 0)
+          return cp - str + 4;
+        if (cp[5] == 0)
+          return cp - str + 5;
+        if (cp[6] == 0)
+          return cp - str + 6;
+        if (cp[7] == 0)
+          return cp - str + 7;
+      }
+	  }
+  }
 }
 
 char *strcpy(char *dst, const char *src) {
