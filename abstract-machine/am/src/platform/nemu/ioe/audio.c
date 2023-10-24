b4eda43cf45f2220ddee8311a09d9221da608412
tracer-ics2023 <tracer@njuics.org>
2023-10-25 01:48:05 +0800

    >  compile NEMU SA23225330 Nops1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:48:05 up 10:21,  1 user,  load average: 0.23, 0.21, 0.18

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -8,33 +8,43 @@
 #define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
 #define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
 
+/*
 typedef struct _AudioData {
   int freq, channels, samples, sbuf_size;
 }_AudioData;
 
 static _AudioData audio; 
+*/
 
 void __am_audio_init() {
+  /*
   audio.freq = inl(AUDIO_FREQ_ADDR);
   audio.channels = inl(AUDIO_CHANNELS_ADDR);
   audio.samples = inl(AUDIO_SAMPLES_ADDR);
   audio.sbuf_size = inl(AUDIO_SBUF_SIZE_ADDR);
+  */
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
   cfg->present = false;
   /* Buf_size will not change. */
+  /*
   cfg->bufsize = audio.sbuf_size;
+  */
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+  /*
   ctrl->freq = audio.freq;
   ctrl->channels = audio.channels;
   ctrl->samples = audio.samples;
+  */
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
+  /*
   stat->count = inl(AUDIO_COUNT_ADDR);
+  */
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
