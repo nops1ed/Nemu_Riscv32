db5dc7a2247709355a798b5a509a7a3b205377a7
tracer-ics2023 <tracer@njuics.org>
2023-10-24 16:13:17 +0800

    >  compile NEMU SA23225330 Nops1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:13:17 up 52 min,  1 user,  load average: 0.07, 0.13, 0.12

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -3,11 +3,7 @@
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
-# define W    400
-# define H    300
-
 void __am_gpu_init() {
-  /*
   //vgactl_port_base[0] = (screen_width() << 16) | screen_height();
   
   int i;
@@ -17,7 +13,6 @@ void __am_gpu_init() {
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
-  */
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -32,17 +27,9 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-  if (w == 0 || h == 0) return;
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  uint32_t *pi = ctl->pixels;
-  int i, j;              
-  for (i = 0; i < h; i ++) {
-    for (j = 0; j < w; j ++) {
-      fb[(y + i) * W + x + j] = pi[i * w + j];
-    }
+  if (ctl->sync) {
+    outl(SYNC_ADDR, 1);
   }
-  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
