7069e4898b4516f27060a117ee0dd72660ba61db
tracer-ics2023 <tracer@njuics.org>
2023-10-25 18:13:12 +0800

    >  compile NEMU SA23225330 Nops1ed Linux Code 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:13:12 up 1 day,  2:46,  1 user,  load average: 0.03, 0.12, 0.12

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -18,8 +18,6 @@
 #include <SDL2/SDL.h>
 
 
-#define min(x,y) ( (x < y) ? x : y )
-
 enum {
   reg_freq,
   reg_channels,
@@ -34,7 +32,6 @@ static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
 static uint32_t sbuf_pos = 0;
 static SDL_AudioSpec s = {};
-static bool is_init = false;
 
 void sdl_audio_callback(void *userdata, uint8_t *stream, int len){
   /* Clear the stream. */
@@ -42,8 +39,8 @@ void sdl_audio_callback(void *userdata, uint8_t *stream, int len){
   uint32_t used_cnt = audio_base[reg_count];
   if(len > used_cnt) len = used_cnt;
   
-  uint32_t sbuf_size = audio_base[reg_sbuf_size]/sizeof(uint8_t);
-  if( (sbuf_pos + len) > sbuf_size ){
+  uint32_t sbuf_size = audio_base[reg_sbuf_size];
+  if(sbuf_pos + len > sbuf_size ){
     SDL_MixAudio(stream, sbuf + sbuf_pos, sbuf_size - sbuf_pos , SDL_MIX_MAXVOLUME);
     SDL_MixAudio(stream +  (sbuf_size - sbuf_pos), sbuf +  (sbuf_size - sbuf_pos), len - (sbuf_size - sbuf_pos) , SDL_MIX_MAXVOLUME);
   }
@@ -56,8 +53,15 @@ void sdl_audio_callback(void *userdata, uint8_t *stream, int len){
 
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
   if(audio_base[reg_init]==1){
-
-    is_init = true;
+    s.format = AUDIO_S16SYS;  // 假设系统中音频数据的格式总是使用16位有符号数来表示
+    s.userdata = NULL;        // 不使用
+    s.freq = audio_base[reg_freq];
+    s.channels = audio_base[reg_channels];
+    s.samples = audio_base[reg_samples];
+    s.callback = sdl_audio_callback;
+    SDL_InitSubSystem(SDL_INIT_AUDIO);
+    SDL_OpenAudio(&s, NULL);
+    SDL_PauseAudio(0);
     audio_base[reg_init] = 0;
   }
 }
@@ -76,14 +80,4 @@ void init_audio() {
   sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
   add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, NULL);
   audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
-
-      s.format = AUDIO_S16SYS;  // 假设系统中音频数据的格式总是使用16位有符号数来表示
-    s.userdata = NULL;        // 不使用
-    s.freq = audio_base[reg_freq];
-    s.channels = audio_base[reg_channels];
-    s.samples = audio_base[reg_samples];
-    s.callback = sdl_audio_callback;
-    SDL_InitSubSystem(SDL_INIT_AUDIO);
-    SDL_OpenAudio(&s, NULL);
-    SDL_PauseAudio(0);
 }
